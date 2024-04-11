c302598070ae8b54de736ca815717137f7c618dd
Mridul Jain <mriduljain202003@gmail.com>
2024-04-12 01:08:26 +0530

    connected to db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL).then((res) => {
+    console.log('Mongo Db connected');
+}).catch((err) => { console.log(err); });
 
 const app = express();
 
